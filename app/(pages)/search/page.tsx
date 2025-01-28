66cee6b880ac8b45fad389807f45f0c668a63257
Code-medic-official <codemedic2@gmail.com>
2025-01-28 15:29:11 +0300

    Fixed Search Page with help from Deepseek

--- app/(pages)/search/page.tsx
+++ app/(pages)/search/page.tsx
@@ -1,3 +1,42 @@
+// "use server";
+
+// import SearchForm from "@/components/forms/SearchForm";
+// import SearchFeed from "@/components/SearchFeed";
+// import { getSearchComments } from "@/lib/actions/comment.action";
+// import { getSearchThreads } from "@/lib/actions/thread.actions";
+// import { getSearchUsers } from "@/lib/actions/user.actions";
+// import { iComment } from "@/lib/database/models/comment.model";
+// import { iThread } from "@/lib/database/models/thread.model";
+// import { iUser } from "@/lib/database/models/user.model";
+
+// export const getSearchQuery = async (q: string) => {
+// 	console.log("searching");
+// 	const threadResults: iThread[] = await getSearchThreads(q);
+// 	const accountResults: iUser[] = await getSearchUsers(q);
+// 	const replyResults: iComment[] = await getSearchComments(q);
+// 	await page(null, { accountResults, replyResults, threadResults });
+// };
+
+// export const dynamic = "force-dynamic";
+
+// // export default async function page(props, {threadResults, accountResults, replyResults}) {
+// export default async function page(props, results) {
+// 	console.log(props);
+
+// 	return (
+// 		<div>
+// 			<SearchForm qChange={getSearchQuery} />
+// 			{true && (
+// 				<>
+// 					<section className="mt-3">
+// 						<SearchFeed results={results} />
+// 					</section>
+// 				</>
+// 			)}
+// 		</div>
+// 	);
+// }
+
 "use server";
 
 import SearchForm from "@/components/forms/SearchForm";
@@ -9,29 +48,31 @@ import { iComment } from "@/lib/database/models/comment.model";
 import { iThread } from "@/lib/database/models/thread.model";
 import { iUser } from "@/lib/database/models/user.model";
 
+// Server action to fetch search results
 export const getSearchQuery = async (q: string) => {
 	console.log("searching");
 	const threadResults: iThread[] = await getSearchThreads(q);
 	const accountResults: iUser[] = await getSearchUsers(q);
 	const replyResults: iComment[] = await getSearchComments(q);
-	await page(null, { accountResults, replyResults, threadResults });
+	return { accountResults, replyResults, threadResults };
 };
 
-export const dynamic = "force-dynamic";
-
-// export default async function page(props, {threadResults, accountResults, replyResults}) {
-export default async function page(props, results) {
-	console.log(props);
+// Page component
+export default async function SearchPage({
+	searchParams,
+}: {
+	searchParams: { q?: string };
+}) {
+	// Fetch search results based on the query parameter
+	const results = searchParams.q ? await getSearchQuery(searchParams.q) : null;
 
 	return (
 		<div>
 			<SearchForm qChange={getSearchQuery} />
-			{true && (
-				<>
-					<section className="mt-3">
-						<SearchFeed results={results} />
-					</section>
-				</>
+			{results && (
+				<section className="mt-3">
+					<SearchFeed results={results} />
+				</section>
 			)}
 		</div>
 	);
--- components/forms/SearchForm.tsx
+++ components/forms/SearchForm.tsx
@@ -2,6 +2,9 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { iComment } from "@/lib/database/models/comment.model";
+import { iThread } from "@/lib/database/models/thread.model";
+import { iUser } from "@/lib/database/models/user.model";
 import { LoaderPinwheel, Search } from "lucide-react";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
@@ -9,9 +12,15 @@ import { useForm } from "react-hook-form";
 export default function SearchForm({
 	qChange,
 }: {
-	qChange: (q: string) => Promise<void>;
+	qChange: (
+		q: string
+	) => Promise<{
+		accountResults: iUser[];
+		replyResults: iComment[];
+		threadResults: iThread[];
+	}>;
 }) {
-	const isPending = false
+	const isPending = false;
 	// const [isPending, startTransition] = useTransition();
 
 	const { register, handleSubmit, watch } = useForm();
