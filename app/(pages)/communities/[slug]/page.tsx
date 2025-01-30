44f7b1180a89b0129a8c3621d38ce8896566500f
Code-medic-official <codemedic2@gmail.com>
2025-01-30 11:03:44 +0300

    Solved Communities router dynamic Page issues

--- app/(pages)/communities/[slug]/page.tsx
+++ app/(pages)/communities/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import { getCommunity } from "@/lib/actions/community.actions";
 import React from "react";
 
+export const dynamic = "force-dynamic";
+
 export default async function page({
 	params,
 }: {
@@ -9,7 +11,7 @@ export default async function page({
 	const { slug } = await params;
 	const community = await getCommunity(slug);
 
-	console.log(community)
+	console.log(community);
 
 	return <div>page</div>;
 }
